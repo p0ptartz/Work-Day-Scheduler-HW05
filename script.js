695479b76d002e4ded8ed1ad7a87fc4e8245d104
p0ptartz <ericwm420@gmail.com>
2023-01-11 22:03:09 -0500

    finished javascript

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
 $(function () {
-// displays the current day in the currentDay id
+  // displays the current day in the currentDay id
   var today = dayjs()
   $("#currentDay").text(today.format("MMM D, YYYY"))
-// calls the changeColor function
+  // calls the changeColor function
   changeColor()
-  saveText ()
+  saveText()
+  displayText()
 
 });
 
@@ -16,56 +17,54 @@ function changeColor() {
   // console.log(hour)
 
   // for each function to grab which timeblock were on
-  $(".time-block").each(function() {
+  $(".time-block").each(function () {
 
     // var time gets current timeblock element, attr id grabs which time block id, split removes the hour- from the id
-    var time = $(this).attr('id').split('-')[1]; 
+    var time = $(this).attr('id').split('-')[1];
     // console.log(time)
     // changes classes which will change bg color based on what the current hour is in relation to the timeblock elements 
-    if (hour < time) { 
-      $(this).removeClass("past present").addClass("future"); 
-    } else if (hour > time) { 
-      $(this).removeClass("future present").addClass("past"); 
-    } else { 
-      $(this).removeClass("past future").addClass("present"); 
+    if (hour < time) {
+      $(this).removeClass("past present").addClass("future");
+    } else if (hour > time) {
+      $(this).removeClass("future present").addClass("past");
+    } else {
+      $(this).removeClass("past future").addClass("present");
     }
   });
 }
 
-function saveText (){
-
-  $(".saveBtn").on("click", function(){
+// function to save the text inputs to local storage
+function saveText() {
+  $(".saveBtn").on("click", function () {
     // grabbing the id of the parent
     var hourEl = $(this).parent().attr('id')
     // console.log(hourEl)
     var textEl = $(this).siblings(".description").val();
-    // console.log(textEl)
+    console.log(textEl)
     localStorage.setItem(hourEl, textEl);
-  })
 
-  $(".description").each(function(){
-    var textEl = $(this).siblings(".description").val();
-    var showText = localStorage.getItem(textEl)
-    $(this).val(showText)
   })
+}
 
+// function to keep text displayed even after refresh
+function displayText() {
+  // this grabs the entire DOM 
+  $(function () {
+    // looping description class 
+    $('.description').each(function () {
+      // grabs description class elements 
+      var hourEl = $(this).parent().attr('id');
+      // console.log(hourEl)
+      // retrieves local storage from key hourEl
+      var savedText = localStorage.getItem(hourEl);
+      $(this).val(savedText);
+    });
+  });
 }
 
 
-  
-  
+// sorry for the ugly code due to the comments.  these comments help me remember what each line does as it takes several hours for me to solve each function.  with so many different attempts i get tripped up with understanding how i got to the solutions.
 
 
-
-
-  // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. HINT: How can the id attribute of each time-block be used to do this?
-  
-  
-// TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. 
-
-// HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? 
-
-// How might the id be useful when saving the description in local storage?
 
 
